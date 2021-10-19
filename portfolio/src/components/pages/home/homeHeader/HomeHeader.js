ae5a38ad1abf9545fd90f126e3530cd0d82152c5
Tonyo Vanov <tonyo_vanov@yahoo.com>
2021-10-18 23:20:17 -0700

    update

--- portfolio/src/components/pages/home/homeHeader/HomeHeader.js
+++ portfolio/src/components/pages/home/homeHeader/HomeHeader.js
@@ -1,31 +1,31 @@
 import "./HomeHeader.css";
 import React from "react";
 import { Link } from "react-router-dom";
-import Particles from "react-particles-js";
+// import Particles from "react-particles-js";
 
-const particleOpt = {
-  particles: {
-    number: {
-      value: 120,
-      density: {
-        enable: true,
-        value_area: 1200,
-      },
-    },
-    line_linked: {
-      shadow: {
-        enable: true,
-        color: "#000",
-        blur: 1,
-      },
-    },
-  },
-};
+// const particleOpt = {
+//   particles: {
+//     number: {
+//       value: 120,
+//       density: {
+//         enable: true,
+//         value_area: 1200,
+//       },
+//     },
+//     line_linked: {
+//       shadow: {
+//         enable: true,
+//         color: "#000",
+//         blur: 1,
+//       },
+//     },
+//   },
+// };
 
 const HomeHeader = () => {
   return (
     <header id="home-header" className="background-position">
-      <Particles params={particleOpt} className="home-particle" />
+      {/* <Particles params={particleOpt} className="home-particle" /> */}
       <div className="wrapper">
         <Link to="./pages/portfolio" className="home-btn middle-positioning">
           <span></span>
